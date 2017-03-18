da946e106110647fe7f8bdb8e7859f251afe3403
NirajVyas <nirajvyas28@hotmail.com>
2017-03-18 18:48:18 +0000

    Basic markup and styling of the header section of the page

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="header">
+      <div className="header overlay">
         <div className="header-inner">
+        <h1>Niraj Vyas</h1>
+        <h2>Web Developer</h2>
           <ul>
-            <li>Terminal</li>
-            <li>Work Examples</li>
-            <li>Contact</li>
+            <li className="terminal"><span>Terminal</span></li>
+            <li className="examples"><span>Work Examples</span></li>
+            <li className="contact"><span>Contact</span></li>
+            <li className="cv"><span>CV</span></li>
           </ul>
         </div>
       </div>
