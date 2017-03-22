82f7a3741049ee4226f430da4ff53e21b207d191
NirajVyas <nirajvyas28@hotmail.com>
2017-03-22 23:04:21 +0000

    logo added

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,12 +7,32 @@ class Header extends Component {
         <div className="header-inner">
         <h1>Niraj Vyas</h1>
         <h2>Web Developer</h2>
+
+        <div className="logo-container">
+          <div className="logo">N<span>V</span></div>
+        </div>        
+
+        <nav>
           <ul>
-            <li className="terminal"><span>Terminal</span></li>
-            <li className="examples"><span>Work Examples</span></li>
-            <li className="contact"><span>Contact</span></li>
-            <li className="cv"><span>CV</span></li>
+            <li className="terminal">
+            <img src="http://placehold.it/350x150>" alt="Terminal icon"/>
+              <span>Terminal</span>
+            </li>
+            <li className="examples">
+            <img src="http://placehold.it/350x150>" alt="Website icon"/>
+              <span>Work</span>
+            </li>
+            <li className="contact">
+            <img src="http://placehold.it/350x150>" alt="Contact icon"/>
+              <span>Contact</span>
+            </li>
+            <li className="cv">
+              <img src="http://placehold.it/350x150>" alt="CV icon"/>
+              <span>CV</span>
+            </li>
           </ul>
+        </nav>
+
         </div>
       </div>
     );
